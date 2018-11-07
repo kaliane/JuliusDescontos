4e5c39811518d685b69446c6e68965ea60756400
levi <levispina@hotmail.com>
2018-11-07 19:33:06 -0200

    ajuste codigo

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from.views import index
 from.views import login
+from.views import cadastro
 
 urlpatterns = [
-	path('',login),
+	path('index', index),
+    path('login', login),
+    path('cadastro', cadastro),
 ]
